c410953bcecbc64af2cb3fb4624e45dbf00fe433
Christopher Bennett <cbcolleg@udel.edu>
2022-07-08 12:47:39 -0400

    merging branches

--- src/components/document-viewer/docUtils.tsx
+++ src/components/document-viewer/docUtils.tsx
@@ -2,8 +2,10 @@ import React from "react";
 
 /**
  * Takes a Document file of XML content and extracts paragraph information
+ * 
  * @param docXML Document file containing XML elements of the uploaded DOCX file
  * @returns Array of Elements representing each paragraph in the DOCX
+ * 
  */
 function extractParagraphs(docXML: Document | undefined): Element[] {
   
