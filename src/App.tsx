c508b8fc2be980a53de99db76ebb57286474b90b
cthacker-udel <cthacker@udel.edu>
2022-07-09 02:26:06 -0400

    [icon-additions] Moved header component to separate folder

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,7 @@ import { FileImport } from "./components/FileImport";
 import { DocViewer } from "./components/document-viewer/DocViewer";
 import { Row, Col } from "react-bootstrap";
 import styles from "./App.module.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimeline } from "@fortawesome/free-solid-svg-icons";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import AppHeader from "./components/App/AppHeader";
 
 /**
  * Root component
@@ -25,49 +23,7 @@ function App() {
 
 	return (
 		<div className="Timeline-site">
-			<div
-				className={"text-center d-flex flex-column justify-content-center pb-4 border-bottom bg-light"}
-			>
-				<div
-					className={`mt-5 h-100 ml-auto d-flex flex-row ${styles.header_title} justify-content-between`}
-				>
-					<div>
-						<span className="fs-4 me-3">Assignment Timeline Maker</span>
-						<span>
-							<FontAwesomeIcon icon={faTimeline} size="2x" />
-						</span>
-					</div>
-					<span className="float-right pe-5 pt-1" role="button">
-						<a
-							className="text-dark"
-							href="https://github.com/CBColleg3/Assignment-Timeline-Maker"
-						>
-							<FontAwesomeIcon icon={faGithub} size="2x" />
-						</a>
-					</span>
-				</div>
-				<div className="text-muted text-wrap w-50 mx-auto text-center mt-3">
-					<span className="fw-bolder">Authors:</span>
-					<a
-						className={` ${styles.author_link} text-decoration-none mx-2 text-secondary`}
-						href="https://github.com/CBColleg3"
-					>
-						Christopher Bennett
-					</a>
-					<a
-						className={` ${styles.author_link} text-decoration-none mx-2 text-secondary`}
-						href="https://github.com/bgallamoza"
-					>
-						Brennan Gallamoza
-					</a>
-					<a
-						className={` ${styles.author_link} text-decoration-none mx-2 text-secondary`}
-						href="https://github.com/cthacker-udel"
-					>
-						Cameron Thacker
-					</a>
-				</div>
-			</div>
+			<AppHeader />
 			<Row className="pt-4">
 				<Col>
 					<SetDateTime
