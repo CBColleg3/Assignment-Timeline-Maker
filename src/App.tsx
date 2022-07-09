890f290526b0899b2f6363f4666b79ec32601d36
cthacker-udel <cthacker@udel.edu>
2022-07-09 00:16:27 -0400

    [icon-additions] Added header element

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
-import React, { useState } from "react";
-import "./App.css";
+import React from "react";
 import "react-datepicker/dist/react-datepicker.css";
 import { Timeline } from "./components/Timeline";
 import type { Task } from "./templates/task";
@@ -7,26 +6,69 @@ import { SetDateTime } from "./components/Date/SetDateTime";
 import { FileImport } from "./components/FileImport";
 import { DocViewer } from "./components/document-viewer/DocViewer";
 import { Row, Col } from "react-bootstrap";
+import styles from "./App.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimeline } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 /**
  * Root component
- * 
+ *
  * @returns {JSX.Element} Main app component
  */
 function App() {
-	//State
-	const [startDate, setStartDate] = useState<Date>(new Date());
-	const [endDate, setEndDate] = useState<Date>(new Date());
-	const [taskArray, setTaskArray] = useState<Task[]>([]);
-	const [fileImported, setFileImported] = useState<boolean>(false);
-	const [docXML, setDocXML] = useState<Document | undefined>(undefined);
+	const [startDate, setStartDate] = React.useState<Date>(new Date());
+	const [endDate, setEndDate] = React.useState<Date>(new Date());
+	const [taskArray, setTaskArray] = React.useState<Task[]>([]);
+	const [fileImported, setFileImported] = React.useState<boolean>(false);
+	const [docXML, setDocXML] = React.useState<Document | undefined>(undefined);
 
 	return (
 		<div className="Timeline-site">
-			<header className="App-header">
-				<p>Assignment Timeline Maker</p>
-			</header>
-			<Row>
+			<div
+				className={"text-center d-flex flex-column justify-content-center pb-4 border-bottom bg-light"}
+			>
+				<div
+					className={`mt-5 h-100 ml-auto d-flex flex-row ${styles.header_title} justify-content-between`}
+				>
+					<div>
+						<span className="fs-4 me-3">Assignment Timeline Maker</span>
+						<span>
+							<FontAwesomeIcon icon={faTimeline} size="2x" />
+						</span>
+					</div>
+					<span className="float-right pe-5 pt-1" role="button">
+						<a
+							className="text-dark"
+							href="https://github.com/CBColleg3/Assignment-Timeline-Maker"
+						>
+							<FontAwesomeIcon icon={faGithub} size="2x" />
+						</a>
+					</span>
+				</div>
+				<div className="text-muted text-wrap w-50 mx-auto text-center mt-3">
+					<span className="fw-bolder">Authors:</span>
+					<a
+						className={` ${styles.author_link} text-decoration-none mx-2 text-secondary`}
+						href="https://github.com/CBColleg3"
+					>
+						Christopher Bennett
+					</a>
+					<a
+						className={` ${styles.author_link} text-decoration-none mx-2 text-secondary`}
+						href="https://github.com/bgallamoza"
+					>
+						Brennan Gallamoza
+					</a>
+					<a
+						className={` ${styles.author_link} text-decoration-none mx-2 text-secondary`}
+						href="https://github.com/cthacker-udel"
+					>
+						Cameron Thacker
+					</a>
+				</div>
+			</div>
+			<Row className="pt-4">
 				<Col>
 					<SetDateTime
 						startDate={startDate}
@@ -51,7 +93,7 @@ function App() {
 						endDate={endDate}
 					/>
 				</Col>
-				<Col column-width="15px">
+				<Col lg={5}>
 					<DocViewer docXML={docXML} fileImported={fileImported} />{" "}
 				</Col>
 			</Row>
--- src/components/document-viewer/DocViewer.tsx
+++ src/components/document-viewer/DocViewer.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import "../../App.css";
 import { extractParagraphs, convertXML2HTML } from "./docUtils";
 
 /**
