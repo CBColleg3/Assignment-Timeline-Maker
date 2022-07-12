11448f96cb8c5d39fa8d52dcc54d563ecc5501b3
cthacker-udel <cthacker@udel.edu>
2022-07-12 17:25:35 -0400

    [organize-helpers] Organized all helper functions into their respective component folders

--- src/helpers/regexFilterTags.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Utility function to aid in the
- *
- * @param text The text to filter with the regex expression
- * @param expr The regex expression executed on the text
- * @returns The result of the regex or undefined if no matches
- */
-export const regexFilterTags = (text: string, expr: RegExp): RegExpExecArray | null => expr.exec(text);
