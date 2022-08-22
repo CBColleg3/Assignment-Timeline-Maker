9756be3a72af8b34d29bd1ec4de4c73113076b1f
bgallamoza <brennan.gallamoza@gmail.com>
2022-08-22 10:08:27 -0400

    re-added old doc-viewer implementation

--- src/helpers/DocViewer/convertXML2HTML.tsx
+++ src/helpers/DocViewer/convertXML2HTML.tsx
@@ -1,133 +1,203 @@
 import React from "react";
-import type { HTMLStyle } from "src/@types";
+import type { HTMLStyle, TaskCollection, Task } from "src/@types";
 import { translateXMLElementStyling } from "./translateXMLElementStyling";
 
-const MIN_PARAGRAPH_CHILD_LENGTH = 1;
-const BASE_INDEX = 0;
-const FLAT_DIMENSION = 2;
-const CONTENT_SLICE_INDEX = 1;
-const MIN_ATTRIBUTE_LENGTH = 0;
-const LIST_ELEMENT_DOES_NOT_EXIST = -1;
-
 /**
- * Utility function for determining whether an element is a space
  *
- * @param childrenAmount The amount of children in the node
- * @returns Whether the children amount designates a space
+ * @param textChunk
+ * @param globalStyleChunk
+ * @returns
  */
-const isSpace = (childrenAmount: number): boolean => childrenAmount === MIN_PARAGRAPH_CHILD_LENGTH;
+function convertTextChunk(textChunk: Element, globalStyleChunk: Element[]): JSX.Element {
+	let output;
+	const style: Record<string, string> = getTextStyle(textChunk);
+	const highlightStyle: Record<string, string> = getHighlightStyle(textChunk);
 
-/**
- * Traverses an xml element recursively and returns all elements
- *
- * @param element The current element we are traversing through
- * @param elements The stack of elements initialized at the root level
- * @returns All the elements within the element
- */
-const traverseXmlTree = (element: Element, elements: Element[] = []): Element[] => {
-	if (!element) {
-		return elements;
+	const textArray = Array.from(textChunk.getElementsByTagName("w:t"));
+
+	if (textArray.length === 0) {
+		return <span>{output}</span>;
+	} else {
+		output = textArray[0].childNodes[0].nodeValue;
 	}
-	const rootElements = [
-		...elements,
-		element,
-		...[...element.children].map((eachChild) => traverseXmlTree(eachChild, elements)).flat(FLAT_DIMENSION),
-	];
-	return [...rootElements];
-};
+
+	return (
+		<span style={highlightStyle}>
+			<span style={style}>{output}</span>
+		</span>
+	);
+}
 
 /**
- * Utility function for generating an html style from a node attribute
  *
- * @param attribute The attribute to convert to HTML style
- * @returns The HTML stylized attribute
+ * @param textChunk
+ * @returns
  */
-const convertAttributeToHtmlStyle = (attribute: Attr): HTMLStyle => {
-	const { name, value } = attribute;
-	return { name, value };
-};
+function getTextStyle(textChunk: Element): Record<string, string> {
+	const style: Record<string, string> = {};
+
+	if (textChunk.getElementsByTagName("w:b").length !== 0) style["font-weight"] = "bold";
+
+	if (textChunk.getElementsByTagName("w:i").length !== 0) {
+		style["font-style"] = "italic";
+	}
+
+	if (textChunk.getElementsByTagName("w:u").length !== 0) {
+		style["text-decoration"] = "underline";
+	}
+
+	if (textChunk.getElementsByTagName("w:strike").length !== 0) {
+		if (style["text-decoration"] !== undefined) {
+			style["text-decoration"] += " line-through";
+		} else {
+			style["text-decoration"] = "line-through";
+		}
+	}
+
+	if (textChunk.getElementsByTagName("w:color").length !== 0) {
+		const color = textChunk.getElementsByTagName("w:color")[0].getAttribute("w:val");
+		console.log(color);
+		if (color !== null) {
+			style["color"] = "#" + color;
+		}
+	}
+
+	return style;
+}
 
 /**
- * Takes an Element containing 'w:p' xml tag information and extracts the text information from it
+ *
+ * @param textChunk
+ * @returns
+ */
+function getHighlightStyle(textChunk: Element): Record<string, string> {
+	const highlightStyle: Record<string, string> = {};
+
+	if (textChunk.getElementsByTagName("w:highlight").length !== 0) {
+		const colorName = textChunk.getElementsByTagName("w:highlight")[0].getAttribute("w:val");
+
+		if (colorName !== null) {
+			if (colorHexs[colorName] !== undefined) {
+				const _opacity = Math.round(Math.min(Math.max(0.5 || 1, 0), 1) * 255);
+				highlightStyle["background-color"] = colorHexs[colorName] + _opacity.toString(16).toUpperCase();
+			}
+		}
+	} else if (textChunk.getElementsByTagName("w:shd").length !== 0) {
+		// highlighting takes precedence over standard color fill
+		const color = textChunk.getElementsByTagName("w:shd")[0].getAttribute("w:fill");
+
+		if (color !== null) {
+			highlightStyle["background-color"] = "#" + color;
+		}
+	}
+
+	return highlightStyle;
+}
+
+/**
+ *
+ * @param globalStyleChunks
+ * @returns
+ */
+function getBackgroundStyle(globalStyleChunks: Element[]): Record<string, string> {
+	const backgroundStyle: Record<string, string> = {};
+
+	if (globalStyleChunks.length === 0) {
+		return backgroundStyle;
+	}
+
+	const parentTag = globalStyleChunks[0].parentElement?.tagName;
+
+	if (parentTag !== undefined) {
+		if (globalStyleChunks[0].getElementsByTagName("w:shd").length !== 0) {
+			const color = globalStyleChunks[0].getElementsByTagName("w:shd")[0].getAttribute("w:fill");
+
+			if (color !== null) {
+				backgroundStyle["background-color"] = "#" + color;
+			}
+		}
+	}
+
+	return backgroundStyle;
+}
+
+/**
+ *
+ * @param formattedContent
+ * @param globalStyleChunk
+ * @returns
+ */
+function styleContent(
+	formattedContent: JSX.Element[],
+	globalStyleChunk: Element[],
+): JSX.Element | JSX.Element[] {
+	const backgroundStyle: Record<string, string> = getBackgroundStyle(globalStyleChunk);
+
+	if (globalStyleChunk.length === 0 || globalStyleChunk[0].getElementsByTagName("w:pStyle").length === 0) {
+		return <span style={backgroundStyle}>{formattedContent}</span>;
+	}
+
+	const tag = globalStyleChunk[0].getElementsByTagName("w:pStyle")[0].getAttribute("w:val");
+
+	if (tag !== undefined) {
+		switch (tag) {
+			case "Heading1":
+				return <h1 style={backgroundStyle}>{formattedContent}</h1>;
+			case "Heading2":
+				return <h2 style={backgroundStyle}>{formattedContent}</h2>;
+			case "Heading3":
+				return <h3 style={backgroundStyle}>{formattedContent}</h3>;
+			case "Heading4":
+				return <h4 style={backgroundStyle}>{formattedContent}</h4>;
+			default:
+				return <span style={backgroundStyle}>{formattedContent}</span>;
+		}
+	} else {
+		return <span style={backgroundStyle}>{formattedContent}</span>;
+	}
+}
+
+/**
+ *
+ * @param {string} textContent textContent of the entire paragraph
+ * @param {Task[]} taskArray
+ * @returns
+ */
+function highlightTask(textContent: string, taskArray: Task[]): Record<string, string> {
+	const style: Record<string, string> = {};
+
+	const task = taskArray.find((task: Task): boolean => task.document.includes(textContent));
+	if (task !== undefined) {
+		console.log("match");
+		style["background-color"] = `rgb(${task.color + 100},100,150)`;
+	}
+
+	return style;
+}
+
+/**
+ * Takes an Element containg 'w:p' xml tag information and extracts the text information from it
  *
  * @param {Element} par xml element representing a 'w:p' xml tag
  * @returns {JSX.Element} <p> html tag containing the text information within the 'w:p' tag
  */
-export const convertXML2HTML = (par: Element): JSX.Element => {
-	const htmlElement = par as HTMLElement;
-	const parChildren = [...htmlElement.children];
-	if (isSpace(parChildren.length)) {
-		return <p />;
-	}
+function convertXML2HTML(par: Element, taskCollection: TaskCollection): JSX.Element {
+	const textChunks = Array.from(par.getElementsByTagName("w:r")); // contains text formatting info
+	const globalStyleChunk = Array.from(par.getElementsByTagName("w:pPr")); // contains additional, more specific styling info for text
+	const textArray = Array.from(par.getElementsByTagName("w:t"));
 
-	// Initialize css object
-	const globalCSS: { [key: string]: string } = {};
-	const contentCSS: { [key: string]: string } = {};
+	const textContent = textArray.reduce(
+		(textContent: string, element: Element) => textContent + element.childNodes[0].nodeValue,
+		"",
+	);
 
-	// Gather nested elements from xml element
-	const globalElements = traverseXmlTree(parChildren[BASE_INDEX]);
-	const contentElements = parChildren
-		.slice(CONTENT_SLICE_INDEX)
-		.map((eachElement) => traverseXmlTree(eachElement))
-		.flat(FLAT_DIMENSION);
-
-	// Get text content from node
-	const content = [...par.getElementsByTagName("w:t")]
-		.map((eachContentElement) => eachContentElement.innerHTML)
-		.join("")
-		.replaceAll("&gt;", ">")
-		.replaceAll("&lt;", "<");
-
-	// Gather all css styling from all global elements
-	const globalStyles = globalElements
-		.map((eachElement) =>
-			[...eachElement.attributes].map((eachAttribute) =>
-				translateXMLElementStyling(eachElement.tagName, convertAttributeToHtmlStyle(eachAttribute)),
-			),
-		)
-		.flat(FLAT_DIMENSION)
-		.filter((elem) => elem.value !== "");
-
-	// Gather all css styling from all content elements
-	const contentStyles = contentElements
-		.map((eachElement) => {
-			if (eachElement.attributes.length > MIN_ATTRIBUTE_LENGTH) {
-				return [...eachElement.attributes].map((eachAttribute) =>
-					translateXMLElementStyling(eachElement.tagName, convertAttributeToHtmlStyle(eachAttribute)),
-				);
-			}
-			return translateXMLElementStyling(eachElement.tagName, { name: "", value: "" });
-		})
-		.flat(FLAT_DIMENSION)
-		.filter((elem) => elem.value !== "");
-
-	// Setting content CSS with styles extracted from content elements
-	contentStyles.forEach((eachStyle) => {
-		contentCSS[eachStyle.name] = eachStyle.value;
-	});
-
-	// Setting global CSS with styles extracted from global elements
-	globalStyles.forEach((eachStyle) => {
-		globalCSS[eachStyle.name] = eachStyle.value;
-	});
-
-	const isListElementPresent = globalElements.findIndex((eachElement) =>
-		eachElement.tagName.includes("w:numPr"),
+	const formattedContent = textChunks.map(
+		(textChunk: Element): JSX.Element => convertTextChunk(textChunk, globalStyleChunk),
 	);
 
 	return (
-		<div style={globalCSS}>
-			<span>
-				{isListElementPresent !== LIST_ELEMENT_DOES_NOT_EXIST ? (
-					<ul>
-						<li>
-							<span style={contentCSS}>{content}</span>
-						</li>
-					</ul>
-				) : (
-					<span style={contentCSS}>{content}</span>
-				)}
-			</span>
-		</div>
+		<p style={highlightTask(textContent, taskCollection.tasks)}>
+			{styleContent(formattedContent, globalStyleChunk)}
+		</p>
 	);
-};
+}
