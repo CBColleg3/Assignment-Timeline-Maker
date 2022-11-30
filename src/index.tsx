c89000aa43d9aa58d4d1c8190e81556878b7349d
cthacker-udel <cthacker@udel.edu>
2022-11-30 03:59:26 -0500

    [improved-docs] Fully documented TaskProvider

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,11 @@ import { App } from "./components";
 import { AssignmentDateInfoProvider, TaskProvider } from "./provider";
 import { FilesProvider } from "./provider/FilesProvider";
 
+/**
+ * The root component, where we render the entire application. As you can see, we are instantiating all the providers here so all the children of it, which is the entire application.
+ * Is able to access the provider's state. Pay attention to the order of the provider's though, as the FilesProvider cannot access anything related to the AssignmentDateInfoContext,
+ * and likewise for the AssignmentDateInfoProvider and accessing the TaskContext state.
+ */
 ReactDOM.render(
 	<React.StrictMode>
 		<FilesProvider>
