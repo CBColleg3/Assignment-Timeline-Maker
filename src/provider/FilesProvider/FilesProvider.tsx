9187671084da8455c77b54fc285e2b33c7e60713
cthacker-udel <cthacker@udel.edu>
2022-11-30 04:10:12 -0500

    [improved-docs] Added documentation to FilesProvider

--- src/provider/FilesProvider/FilesProvider.tsx
+++ src/provider/FilesProvider/FilesProvider.tsx
@@ -8,22 +8,43 @@ type FilesProviderProperties = {
 };
 
 /**
- * HOC component utilizing and properly memoizing all the functional props being sent to it's children
+ * HOC that passes down the task context to any child.
+ * - **Read more about HOC components: https://reactjs.org/docs/higher-order-components.html.**
+ * - **Read more about context: https://reactjs.org/docs/context.html**
  *
- * @param properties - The properties of this Provider HOC component
- * @param properties.children - The children that this HOC component wraps around
- * @returns - The HOC provider component that provides all children access to it's state
+ * @param props The properties of the component, read more about properties here: https://reactjs.org/docs/components-and-props.html
+ * @param props.children The child component that will be receiving the files context, read more about the special children prop here: https://reactjs.org/docs/composition-vs-inheritance.html *(Containment section)*
+ * @returns The wrapped child component
  */
 export const FilesProvider = ({ children }: FilesProviderProperties): JSX.Element => {
+	/**
+	 * The list of files the user currently has imported. Initialized as an empty list to avoid any bugs with undefined. Is populated as the user adds files from the upload component, or
+	 * removes files from the 
+	 */
 	const [files, setFiles] = React.useState<File[]>([]);
+	/**
+	 * The currently selected file. The file which the user selects from the list of files, highlighted in blue as soon as the user selects it.
+	 */
 	const [selectedFile, setSelectedFile] = React.useState<File>();
+	/**
+	 * The parsed text of the file, utilized in places where the tasks need to be generated, or generally when the file text is required.
+	 */
 	const [selectedFileText, setSelectedFileText] = React.useState<string>();
+	/**
+	 * The XML of the selected file, used in the DocViewer component to render the selected file.
+	 */
 	const [selectedFileXML, setSelectedFileXML] = React.useState<Document | undefined>(undefined);
+	/**
+	 * Boolean indicator when the user is removing a file, removes the file from the state.
+	 */
 	const [removingFile, setRemovingFile] = React.useState<boolean>(false);
 
+	/**
+	 * The React useEffect hook that fires when the state `removingFile` changes. This is because the `removingFile` state only changes when the user is confirmed to have removed
+	 * a file from the list of files displayed in the top-center of the screen. When that occurs, all data related to the file is removed. Read more about the `useEffect` hook here: https://reactjs.org/docs/hooks-effect.html
+	 */
 	React.useEffect(() => {
 		if (removingFile) {
-			console.log("resetting all values");
 			setSelectedFileXML(undefined);
 			setRemovingFile(false);
 			setSelectedFile(undefined);
@@ -31,23 +52,31 @@ export const FilesProvider = ({ children }: FilesProviderProperties): JSX.Elemen
 		}
 	}, [removingFile]);
 
+	/**
+	 * 	 * This is a little more complicated then the useEffects above. This is using the `useMemo` hook, which is a powerful hook if used correctly. The general standard practice is, when dealing with
+	 * objects, and specifically using them to supply the Provider a value. You must memoize them or else it results in lots of unnecessary re-renders. This is basically, memoizing the functional props of
+	 * the provider, and making it so whenever we try to recalculate the value of `functionalProps`, we check if the dependency is completely different from the past one, and that the value of the dependency is not
+	 * the same as the one before the changed one. If that is the case, then we already have calculated the value, so therefore we just return the value without running any complex computations. This is especially helpful for
+	 * objects. React has a tendency to treat values as different by examining their memory addresses, an object can have completely the same values, but if it differs in the memory address, then React treats it as a different value.
+	 * You can read more about the useMemo hook here: https://reactjs.org/docs/hooks-reference.html#usememo
+	 * --
+	 * Specifically, this property is memoized to prevent unecessary renders where the object is technically different but the contents are different, which is a common practice when utilizing objects
+	 * and Provider values.
+	 */
 	const functionalProps: Partial<iFilesContext> = React.useMemo(
 		() => ({
 			addFile: (_file: File) => setFiles((oldFiles: File[]) => [...oldFiles, _file]),
 			addFiles: (_files: File[]) => setFiles((oldFiles: File[]) => [...oldFiles, ..._files]),
 			clearFiles: () => setFiles([]),
 			removeFileByIndex: (_index: number): void => {
-				console.log("calling remove index");
 				setRemovingFile(true);
 				setFiles((oldFiles: File[]) => oldFiles.filter((_, index) => index !== _index));
 			},
 			removeFileByName: (_name: string): void => {
-				console.log("calling remove name");
 				setRemovingFile(true);
 				setFiles((oldFiles: File[]) => oldFiles.filter((eachFile) => eachFile.name !== _name));
 			},
 			updateSelectedFile: (_index: number): void => {
-				console.log("updating file");
 				setSelectedFile(files[_index]);
 				readFile(files[_index])
 					.then((result: string | undefined) => {
@@ -63,6 +92,12 @@ export const FilesProvider = ({ children }: FilesProviderProperties): JSX.Elemen
 		[files],
 	);
 
+	/**
+	 * We apply the same practice as described above, and make sure that we memoize our Provider value, **especially** if it is an object. This will save us a ton of unnecessary renders.
+	 * --
+	 * What this object, `filesMemo` is specifically doing, is combining both the `functionalProps` described above, with the local state of this provider. They combine together to form
+	 * a fully complete iFilesContext value. Which we then pass into the Provider's value, for the children to consume it.
+	 */
 	const filesMemo: iFilesContext = React.useMemo(
 		() => ({
 			...(functionalProps as unknown as iFilesContext),
@@ -74,5 +109,8 @@ export const FilesProvider = ({ children }: FilesProviderProperties): JSX.Elemen
 		[files, functionalProps, selectedFile, selectedFileText, selectedFileXML],
 	);
 
+	/**
+	 * Returning FilesContext.Provider that is providing the memoized object as the value to all it's children.
+	 */
 	return <FilesContext.Provider value={filesMemo}>{children}</FilesContext.Provider>;
 };
