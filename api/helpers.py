4c17a80ed2ec15a967a979d4d3c4f09230fd7d5a
bgallamoza <brennan.gallamoza@gmail.com>
2022-11-30 19:48:50 -0500

    resolved data parsing issue in ML api

--- api/helpers.py
+++
@@ -1,7 +0,0 @@
-def format_response(info_type: str) -> str:
-    if info_type == "animal":
-        return "Animals are really cool!"
-    elif info_type == "color":
-        return "Red is a very strong color!"
-    else:
-        return "Emotions affect the way you learn!"
--- api/utils.py
+++ api/utils.py
@@ -2,9 +2,8 @@ from muss.simplify import ALLOWED_MODEL_NAMES, simplify_sentences
 from muss.utils.helpers import read_lines
 from typing import List
 
-def simplify_text(text: str, model_name:str='muss_en_wikilarge_mined') -> List[str]:
-    text = text.strip()
-    sentences = text.split("\n")
+def simplify_text(text: List[str], model_name:str='muss_en_wikilarge_mined') -> List[str]:
+    sentences = [sentence.strip() for sentence in text]
     pred_sentences = simplify_sentences(sentences, model_name)
 
     return pred_sentences
